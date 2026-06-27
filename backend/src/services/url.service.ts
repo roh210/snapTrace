15aa9084218725c5cbbe706f4f538cf26b85a4a5
roh210 <roheena.s786@gmail.com>
2026-06-27 14:55:11 +0100

    fix: prisma client generation using default output

--- backend/src/services/url.service.ts
+++ backend/src/services/url.service.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "../../generated/prisma"
+import { Prisma } from "@prisma/client"
 import { prisma } from "../config/db"
 import { AppError } from "../utils/AppError"
 import { generateShortCode } from "../utils/base62"
